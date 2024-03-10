afd99f5173138ce614f72e51003a3e348b170cae
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-03-09 22:58:49 -0500

    Refactor entry point

--- app.js
+++ app.js
@@ -1,79 +1,73 @@
-'use strict';
+"use strict";
 
-require('dotenv').config()
-var createError = require('http-errors');
-const express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-const session = require('express-session');
+require("dotenv").config();
+
+var createError = require("http-errors");
+const express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const app = express();
-const mongoose = require('mongoose');
-const connectDatabase = require('./lib/connect-mongoose');
-const errorhandler = require('errorhandler');
-const AdvertController = require('./controllers/AdvertController');
-const validateToken = require('./middleware/validatetoken');
 
-const passportconfig = require('./config/passport-config');
-const User = require('./models/user');
+// CONFIG
+const passportconfig = require("./config/passport-config");
 const fileUpload = require("express-fileupload");
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-const registerRouter = require('./routes/register');
-const loginRouter = require('./routes/login');
-
-const TagsController=require('./controllers/TagsController');
-
 passportconfig();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// MIDDELWARE
+const validateToken = require("./middleware/validatetoken");
 
-app.use(logger('dev'));
+// CONTROLLERS
+const AdvertController = require("./controllers/AdvertController");
+const RegisterController = require("./controllers/RegisterController");
+const LoginController = require("./controllers/LoginController");
+const TagsController = require("./controllers/TagsController");
+
+// DB CONNECTION
+require("./lib/connect-mongoose");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
-
-//Token to validate the user
-app.use('/api/advert', validateToken);
-
-//instances 
+//=========== CONTROLLERS INSTANCES =============
 const advertController = new AdvertController();
 const tagsController = new TagsController();
+const registerController = new RegisterController();
+const loginController = new LoginController();
 
-app.get('/api/adverts', advertController.get);
-app.get('/api/id/:id', advertController.getById);
-app.post('/api/advert/new', advertController.post);
-app.delete('/api/advert/:id', advertController.delete);
-app.put('/api/advert/:id', advertController.put);
-app.get('/api/tags', tagsController.get);
+//=========== ROUTER =============
+// REGISTER
+app.post("/api/register", registerController.create);
+// LOGIN
+app.post("/api/login", loginController.login);
+// ADVERTS
+app.get("/api/adverts", advertController.get);
+app.get("/api/advert/id/:id", advertController.getById);
+app.post("/api/advert/new", validateToken, advertController.post);
+app.delete("/api/advert/:id", validateToken, advertController.delete);
+app.put("/api/advert/:id", validateToken, advertController.put);
+// TAGS
+app.get("/api/tags", validateToken, tagsController.get);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+//=========== CATCH 404 =============
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+//=========== ERROR HANDLER =============
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
