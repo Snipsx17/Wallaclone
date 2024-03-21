164bf5d6e9e263c5714eba6236e18d8cdc0a099f
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-03-21 10:32:08 -0400

    Fix user password hash

--- initDB.js
+++ initDB.js
@@ -1,5 +1,6 @@
 "use strict";
 const readLine = require("node:readline");
+const bcrypt = require('bcrypt');
 const initData = require("./initialData.json");
 const dbConnection = require("./lib/connect-mongoose");
 const User = require('./models/user');
@@ -36,7 +37,10 @@ async function initTags() {
 async function initUsers() {
   const deleted = await User.deleteMany();
   console.log(`${deleted.deletedCount} user were deleted`);
-
+  const users = initData.users;
+  users[0].password = await bcrypt.hash(users[0].password, 10);
+  users[1].password = await bcrypt.hash(users[1].password, 10);
+  
   const inserted = await User.insertMany(initData.users);
   const usersIds = inserted.map(user => user._id.toString());
   console.log(`${inserted.length} user created.`);
@@ -52,7 +56,6 @@ async function initAdverts(usersIds) {
     return advert;
   });
 
-  console.log(advertsWithOwner);
   const inserted = await Advert.insertMany(advertsWithOwner);
   console.log(`${inserted.length} adverts created.`);
 }
